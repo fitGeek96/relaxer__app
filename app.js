451820ee7f9a45aefcabaee0e17f71fc7239f679
fitgeek96 <fitgeek96@gmail.com>
2021-03-10 17:34:19 +0200

    Breath Animation with JS trigger

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //jshint esversion:9
+const containerEl = document.getElementById('container');
+const textEl = document.getElementById('text');
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breathAnimation() {
+    textEl.innerHTML = 'Breathe In!';
+    containerEl.className = 'container grow';
+    setTimeout(() => {
+        text.innerHTML = 'Hold';
+
+        setTimeout(() => {
+            text.innerHTML = 'Breathe Out!';
+            containerEl.className = 'container shrink';
+        }, holdTime);
+    }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
